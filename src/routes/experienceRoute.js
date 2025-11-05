f6b94a92058ec2a571b6e5d9a2b52b5207eb1606
ifnuu01 <ifnuu01@gmail.com>
2025-11-05 19:01:12 +0800

    change name import path

--- src/routes/experienceRoute.js
+++ src/routes/experienceRoute.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { getExperiences, getExperienceById, createExperience, updateExperience, deleteExperience } from '../controllers/experienceController.js';
-import { validationExperience, handleValidationErrors } from '../middlewares/validation.js';
+import { getAllExperiences, getExperienceById, createExperience, updateExperience, deleteExperience } from '../controllers/experienceController.js';
+import { validateExperience, handleValidationErrors } from '../middlewares/validation.js';
 
 const router = express.Router();
 
-router.get('/', getExperiences);
+router.get('/', getAllExperiences);
 router.get('/:id', getExperienceById);
-router.post('/', validationExperience, handleValidationErrors, createExperience);
-router.put('/:id', validationExperience, handleValidationErrors, updateExperience);
+router.post('/', validateExperience, handleValidationErrors, createExperience);
+router.put('/:id', validateExperience, handleValidationErrors, updateExperience);
 router.delete('/:id', deleteExperience);
 
 export default router;
--- src/routes/techRoute.js
+++ src/routes/techRoute.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { getTechs, getTechById, createTech, updateTech, deleteTech } from '../controllers/techController.js';
-import { validationTech, handleValidationErrors } from '../middlewares/validation.js';
+import { getAllTechs, getTechById, createTech, updateTech, deleteTech } from '../controllers/techController.js';
+import { validateTech, handleValidationErrors } from '../middlewares/validation.js';
 
 const router = express.Router();
 
-router.get('/', getTechs);
+router.get('/', getAllTechs);
 router.get('/:id', getTechById);
-router.post('/', validationTech, handleValidationErrors, createTech);
-router.put('/:id', validationTech, handleValidationErrors, updateTech);
+router.post('/', validateTech, handleValidationErrors, createTech);
+router.put('/:id', validateTech, handleValidationErrors, updateTech);
 router.delete('/:id', deleteTech);
 
 export default router;
