60724febff3e81e71766d964d92479e0798fc9e5
ifnuu01 <ifnuu01@gmail.com>
2025-11-21 15:33:57 +0800

    feat: add limiter

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './src/config/db.js';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 // routes
 import categoryRoute from './src/routes/categoryRoute.js';
@@ -19,7 +20,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 15,
+    message: 'Terlalu banyak permintaan dari IP ini, silakan coba lagi setelah 15 menit'
+});
 app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json({ extended: true, limit: '10mb' }));
@@ -36,7 +41,7 @@ app.use('/api/experiences', experienceRoute);
 app.use('/api/blogs', blogRoute);
 app.use('/api/projects', projectRoute);
 app.use('/api/certificates', certificateRoute);
-app.use('/api/auth', authRoute);
+app.use('/api/auth', limiter, authRoute);
 
 // 404
 app.use((req, res) => {
